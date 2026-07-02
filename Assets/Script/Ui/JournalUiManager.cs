aeb44181be0ef73a98c3cd63b9e880fc2bbbf041
zippy92 <127176255+zippy92@users.noreply.github.com>
2026-07-02 22:04:48 +0800

    Added Tasks Page in Journal

--- Assets/Script/Ui/JournalUiManager.cs
+++ Assets/Script/Ui/JournalUiManager.cs
@@ -16,10 +16,21 @@ public class JournalUiManager : MonoBehaviour
     public Tab[] tabs;
     private int currentTabIndex = -1;
 
+    [System.Serializable]
+    public class Page
+    {
+        public Button pageButton;
+        public GameObject pageContainer;
+        public Image pageImage;
+        public RectTransform RootLayout;
+        public bool isJournal;
+    }
+
+    public Page[] pages;
+    private int currentPage = -1;
     private void OnEnable()
     {
-        JournalItemSetup();
-        JournalTabSetup();
+        JournalPageSetup();
     }
 
     private void JournalTabSetup()
@@ -69,9 +80,81 @@ public class JournalUiManager : MonoBehaviour
 
         currentTabIndex = index;
     }
+    public void OpenPage(int index)
+    {
+        if (index < 0 || index >= pages.Length || pages == null || pages.Length == 0)
+            return;
+
+        for (int i = 0; i < pages.Length; i++)
+        {
+            //if (tabs[i].IsUnityNull() || tabs[i].container.IsUnityNull()) continue;
+            //tabs[i].container.SetActive(i == index);
+
+            if (pages[i] == null) continue;
+
+            // Show/hide container
+            if (pages[i].pageContainer != null)
+            {
+                pages[i].pageContainer.SetActive(i == index);
+                
+            }
+            if (pages[i].RootLayout != null)
+            {
+                LayoutRebuilder.ForceRebuildLayoutImmediate(pages[i].RootLayout);
+            }
+                //pages[i].pageContainer.SetActive(i == index);
+
+            // Change tab color
+            if (pages[i].pageImage != null)
+                pages[i].pageImage.color = (i == index)
+                    ? Color.white
+                    : Color.gray;
+            if (pages[i].isJournal)
+            {
+                JournalItemSetup();
+                JournalTabSetup();
+            }
+
+            //StartCoroutine(RebuildLayout(pages[i].pageContainer));
+            //else
+            //{
+            //    RectTransform rootObject = pages[i].pageContainer.GetComponent<RectTransform>();
+            //    Debug.Log("Rebuilding Layout");
+            //    LayoutRebuilder.ForceRebuildLayoutImmediate(rootObject);
+            //}
+        }
+
+        currentPage = index;
+        //StartCoroutine(RebuildLayout(pages[index].pageContainer));
+    }
+
+    private void JournalPageSetup()
+    {
+        if (pages == null || pages.Length == 0)
+        {
+            Debug.LogWarning("No pages");
+            return;
+        }
+
+        for (int i = 0; i < pages.Length; i++)
+        {
+            int index = i;
+            if (pages[i].pageButton != null)
+            {
+                pages[i].pageButton.onClick.AddListener(() => OpenPage(index));
+            }
+            else
+            {
+                Debug.LogWarning($"Tab button for index {index} is not assigned. Please assign a Button reference to tabButton in the inspector for this tab.");
+            }
+        }
+        OpenPage(0);
+    }
 
     public void JournalItemSetup()
     {
         //setup the item over here...
     }
+
+    
 }

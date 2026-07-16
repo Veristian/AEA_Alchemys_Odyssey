c8a017ef7737a5b4e363ed93b955622594992345
Veristian <veristianjarvico2@gmail.com>
2026-07-16 21:45:40 +0800

    herb manager

--- Assets/Script/Gathering/Herb.cs
+++ Assets/Script/Gathering/Herb.cs
@@ -1,72 +1,260 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// #if UNITY_EDITOR
+// using UnityEditor;
+// #endif
+// [RequireComponent(typeof(SphereCollider))]
+// public class Herb : MonoBehaviour, IInteractable
+// {
+//     [SerializeField] private IngredientData ingredientData;
+//     [SerializeField] private GameObject herbModel;
+//     [SerializeField] private ParticleSystem CollectedEffect;
+//     private bool isTaken;
+//     private int dayTaken;
+//     SphereCollider col;
+
+//     [SerializeField, HideInInspector]
+//     private string herbID;
+
+//     #if UNITY_EDITOR
+//     private void OnValidate()
+//     {
+//         if (string.IsNullOrEmpty(herbID))
+//         {
+//             herbID = GUID.Generate().ToString();
+//             EditorUtility.SetDirty(this);
+//         }
+//     }
+//     #endif
+//     private void Start()
+//     {
+//         GetComponent<Collider>().isTrigger = true;
+
+//         var state = HerbManager.Instance.GetState(herbID);
+
+//         isTaken = state.isTaken;
+//         dayTaken = state.dayTaken;
+
+//         if (isTaken && dayTaken < DayManager.Instance.Day)
+//         {
+//             RefreshHerb();
+//         }
+//         else
+//         {
+//             herbModel.SetActive(!isTaken);
+//         }
+//     }
+//     public string text
+//     {
+//         get
+//         {
+//             return "Collect";
+//         }
+//     }
+//     public bool interactable
+//     {
+//         get
+//         {
+//             return !isTaken;
+//         }
+//     }
+//     public void Interact(GameObject interactor)
+//     {
+//         TakeHerb();
+//     }
+
+//     // void TakeHerb()
+//     // {
+//     //     if (isTaken) return;
+//     //     isTaken = true;
+//     //     dayTaken = DayManager.Instance.Day;
+        
+//     //     herbModel?.SetActive(false);
+//     //     if (ingredientData)
+//     //     {
+//     //         InventoryManager.Instance.AddIngredient(ingredientData);
+//     //         if (PickupPopupManager.Instance != null)
+//     //         {
+//     //             PickupPopupManager.Instance.ShowPickup(ingredientData);
+//     //             CollectedEffect.Play();
+//     //             AudioController.Instance.PlaySFX("PickUp");
+//     //         }
+//     //     }
+        
+            
+//     // }
+//     void TakeHerb()
+//     {
+//         if (isTaken) return;
+
+//         isTaken = true;
+//         dayTaken = DayManager.Instance.Day;
+
+//         HerbManager.Instance.SetState(herbID, true, dayTaken);
+
+//         herbModel?.SetActive(false);
+
+//         if (ingredientData)
+//         {
+//             InventoryManager.Instance.AddIngredient(ingredientData);
+
+//             PickupPopupManager.Instance?.ShowPickup(ingredientData);
+//             CollectedEffect.Play();
+//             AudioController.Instance.PlaySFX("PickUp");
+//         }
+//     }
+
+//     // void RefreshHerb()
+//     // {
+//     //     isTaken = false;
+//     //     herbModel?.SetActive(true);
+//     // }
+//     void RefreshHerb()
+//     {
+//         isTaken = false;
+//         herbModel?.SetActive(true);
+
+//         HerbManager.Instance.SetState(herbID, false, 0);
+//     }
+
+//     // void OnEnable()
+//     // {
+//     //     if (isTaken && dayTaken < DayManager.Instance.Day)
+//     //     {
+//     //         RefreshHerb();
+//     //     }
+//     // }
+//     void OnEnable()
+//     {
+//         if (HerbManager.Instance == null)
+//             return;
+
+//         var state = HerbManager.Instance.GetState(herbID);
+
+//         isTaken = state.isTaken;
+//         dayTaken = state.dayTaken;
+
+//         if (isTaken && dayTaken < DayManager.Instance.Day)
+//         {
+//             RefreshHerb();
+//         }
+//     }
+
+// }
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+#endif
+
 [RequireComponent(typeof(SphereCollider))]
 public class Herb : MonoBehaviour, IInteractable
 {
+    [Header("Save")]
+    [SerializeField]
+    private string herbID;
+
+    [Header("References")]
     [SerializeField] private IngredientData ingredientData;
     [SerializeField] private GameObject herbModel;
-    [SerializeField] private ParticleSystem CollectedEffect;
+    [SerializeField] private ParticleSystem collectedEffect;
+
     private bool isTaken;
     private int dayTaken;
-    SphereCollider col;
 
     private void Start()
     {
-        GetComponent<Collider>().isTrigger = true;  
+        GetComponent<Collider>().isTrigger = true;
+
+        LoadState();
     }
-    public string text
+
+    private void OnEnable()
     {
-        get
+        if (HerbManager.Instance == null)
+            return;
+
+        LoadState();
+    }
+
+    private void LoadState()
+    {
+        HerbStateData state = HerbManager.Instance.GetState(herbID);
+
+        isTaken = state.isTaken;
+        dayTaken = state.dayTaken;
+
+        if (isTaken && dayTaken < DayManager.Instance.Day)
         {
-            return "Collect";
+            RefreshHerb();
+        }
+        else
+        {
+            herbModel?.SetActive(!isTaken);
         }
     }
-    public bool interactable
-    {
-        get
-        {
-            return !isTaken;
-        }
-    }
+
+    public string text => "Collect";
+
+    public bool interactable => !isTaken;
+
     public void Interact(GameObject interactor)
     {
         TakeHerb();
     }
 
-    void TakeHerb()
+    private void TakeHerb()
     {
-        if (isTaken) return;
+        if (isTaken)
+            return;
+
         isTaken = true;
         dayTaken = DayManager.Instance.Day;
-        
+
+        HerbManager.Instance.SetState(herbID, true, dayTaken);
+
         herbModel?.SetActive(false);
-        if (ingredientData)
+
+        if (ingredientData != null)
         {
             InventoryManager.Instance.AddIngredient(ingredientData);
-            if (PickupPopupManager.Instance != null)
-            {
-                PickupPopupManager.Instance.ShowPickup(ingredientData);
-                CollectedEffect.Play();
-                AudioController.Instance.PlaySFX("PickUp");
-            }
+
+            PickupPopupManager.Instance?.ShowPickup(ingredientData);
+
+            if (collectedEffect != null)
+                collectedEffect.Play();
+
+            AudioController.Instance.PlaySFX("PickUp");
         }
-        
-            
     }
 
-    void RefreshHerb()
+    private void RefreshHerb()
     {
         isTaken = false;
+        dayTaken = 0;
+
+        HerbManager.Instance.SetState(herbID, false, 0);
+
         herbModel?.SetActive(true);
     }
 
-    void OnEnable()
+#if UNITY_EDITOR
+    private void OnValidate()
     {
-        if (isTaken && dayTaken < DayManager.Instance.Day)
+        // Don't assign IDs to the prefab asset itself
+        if (PrefabUtility.IsPartOfPrefabAsset(gameObject))
+            return;
+
+        if (string.IsNullOrEmpty(herbID))
         {
-            RefreshHerb();
+            herbID = GUID.Generate().ToString();
+            EditorUtility.SetDirty(this);
+            EditorSceneManager.MarkSceneDirty(gameObject.scene);
         }
     }
-
+    
+#endif
 }
---
+++ Assets/Script/Gathering/HerbManager.cs
@@ -0,0 +1,230 @@
+// using System;
+// using System.Collections.Generic;
+// using UnityEngine;
+// [Serializable]
+// [Metadata("HerbSaveData")]
+// public class HerbSaveData
+// {
+//     public List<HerbStateData> herbs = new List<HerbStateData>();
+// }
+
+// [Serializable]
+// public class HerbStateData
+// {
+//     public string herbID;
+//     public bool isTaken;
+//     public int dayTaken;
+// }
+// public class HerbManager : MonoBehaviour
+// {
+//     public static HerbManager Instance { get; private set; }
+
+//     [Serializable]
+//     public class HerbState
+//     {
+//         public bool isTaken;
+//         public int dayTaken;
+//     }
+
+//     [SerializeField]
+//     private Dictionary<string, HerbState> herbStates = new();
+
+//     private void Awake()
+//     {
+//         if (Instance != null && Instance != this)
+//         {
+//             Destroy(gameObject);
+//             return;
+//         }
+
+//         Instance = this;
+//     }
+
+//     public HerbState GetState(string herbID)
+//     {
+//         if (!herbStates.TryGetValue(herbID, out HerbState state))
+//         {
+//             state = new HerbState();
+//             herbStates.Add(herbID, state);
+//         }
+
+//         return state;
+//     }
+
+//     public void SetState(string herbID, bool taken, int day)
+//     {
+//         HerbState state = GetState(herbID);
+//         state.isTaken = taken;
+//         state.dayTaken = day;
+//     }
+
+//     public Dictionary<string, HerbState> GetAllStates()
+//     {
+//         return herbStates;
+//     }
+
+//     public void SetAllStates(Dictionary<string, HerbState> states)
+//     {
+//         herbStates = states ?? new Dictionary<string, HerbState>();
+//     }
+
+//     private void Save()
+//     {
+//         HerbSaveData save = new();
+
+//         foreach (var pair in HerbManager.Instance.GetAllStates())
+//         {
+//             save.herbs.Add(new HerbEntry
+//             {
+//                 herbID = pair.Key,
+//                 taken = pair.Value.isTaken,
+//                 dayTaken = pair.Value.dayTaken
+//             });
+//         }
+//     }
+
+//     private void Load()
+//     {
+//         Dictionary<string, HerbManager.HerbState> states = new();
+
+//         foreach (var herb in save.herbs)
+//         {
+//             states.Add(herb.herbID, new HerbManager.HerbState
+//             {
+//                 isTaken = herb.taken,
+//                 dayTaken = herb.dayTaken
+//             });
+//         }
+
+//         HerbManager.Instance.SetAllStates(states);
+//     }
+// }
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+[Serializable]
+public class HerbStateData
+{
+    public string herbID;
+    public bool isTaken;
+    public int dayTaken;
+}
+
+[Serializable]
+[Metadata("HerbSaveData")]
+public class HerbSaveData
+{
+    public List<HerbStateData> herbs = new();
+}
+
+public class HerbManager : Singleton<HerbManager>
+{
+
+    private Dictionary<string, HerbStateData> herbStates = new();
+
+    private const string herbStateFileName = "PlayerHerbStateData";
+
+    protected override void Awake()
+    {
+        transform.parent = null;
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject); 
+            return;
+        }
+        base.Awake();
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        Load();
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneUnloaded += OnSceneUnloading;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneUnloaded -= OnSceneUnloading;
+    }
+
+    public HerbStateData GetState(string herbID)
+    {
+        if (!herbStates.TryGetValue(herbID, out HerbStateData state))
+        {
+            state = new HerbStateData
+            {
+                herbID = herbID,
+                isTaken = false,
+                dayTaken = 0
+            };
+
+            herbStates.Add(herbID, state);
+        }
+
+        return state;
+    }
+
+    public void SetState(string herbID, bool taken, int dayTaken)
+    {
+        HerbStateData state = GetState(herbID);
+        state.isTaken = taken;
+        state.dayTaken = dayTaken;
+    }
+
+    public HerbSaveData GetSaveData()
+    {
+        HerbSaveData save = new();
+
+        foreach (var state in herbStates.Values)
+            save.herbs.Add(state);
+
+        return save;
+    }
+
+    public void LoadSaveData(HerbSaveData save)
+    {
+        herbStates.Clear();
+
+        if (save == null)
+            return;
+
+        foreach (var herb in save.herbs)
+        {
+            herbStates[herb.herbID] = herb;
+        }
+    }
+
+    private void OnSceneUnloading(Scene scene)
+    {
+        Save();
+    }
+
+    public void Save()
+    {
+        HerbSaveData data = new();
+
+        foreach (var herb in herbStates.Values)
+            data.herbs.Add(herb);
+
+        DataManager.Instance.SaveToFile(herbStateFileName, data);
+    }
+
+    public void Load()
+    {
+        herbStates.Clear();
+
+        HerbSaveData data = DataManager.Instance.LoadFromFile(
+            herbStateFileName,
+            () => new HerbSaveData());
+
+        foreach (var herb in data.herbs)
+            herbStates[herb.herbID] = herb;
+    }
+
+
+}
